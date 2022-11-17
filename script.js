e5e1e0614022499e755ba73efe5fb6677b1a49d0
RicardoBachmann <ricardo.bachmann@icloud.com>
2022-11-17 16:56:12 +0100

    generatePassword() works and generate a random password  with 15 characters

--- script.js
+++ script.js
@@ -2,8 +2,26 @@ const generateBtn = document.getElementById("generate-btn")
 
 generateBtn.addEventListener("click", generatePassword) 
   
+console.log(generatePasswords())
 
 
 function generatePassword() {
-    console.log("clicked")
+    const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+    const numbers = "1234567890"
+    const mathSymbols = "</>*:-+!?="
+
+    let characters = upperCase + lowerCase + numbers + mathSymbols
+    characters = characters.split("")
+    
+    let password = "" 
+    
+    for (let i = 0 ; i < 15; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length)
+        password += characters[randomIndex]
+    }
+    console.log(password)
+    
+    return password
+    
 }
