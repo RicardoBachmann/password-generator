392caa8d17679268d2057ca49ae06a5bf206cb7d
RicardoBachmann <ricardo.bachmann@icloud.com>
2022-11-17 17:47:51 +0100

    The generatePassword() based on the number of password elements is not working

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const generateBtn = document.getElementById("generate-btn")
+const passwordList = document.getElementById("password-list")
+const passwordEl = document.querySelectorAll("password")
+const decrementBtn = document.getElementById("decrement-btn")
+const incrementBtn = document.getElementById("increment-btn")
 
 generateBtn.addEventListener("click", generatePassword) 
-  
-console.log(generatePasswords())
+
+
 
 
 function generatePassword() {
@@ -10,7 +14,7 @@ function generatePassword() {
     const lowerCase = "abcdefghijklmnopqrstuvwxyz"
     const numbers = "1234567890"
     const mathSymbols = "</>*:-+!?="
-
+    
     let characters = upperCase + lowerCase + numbers + mathSymbols
     characters = characters.split("")
     
@@ -20,8 +24,21 @@ function generatePassword() {
         let randomIndex = Math.floor(Math.random() * characters.length)
         password += characters[randomIndex]
     }
+    
     console.log(password)
-    
     return password
-    
 }
+/*
+function generatePassword() {
+    let passwords = []
+    for (let i = 0; i < passwordEl.length; i++) {
+        passwords.push(generatePassword())
+    }
+    
+    for (let i = 0; i < passwords.length; i++) {
+        passwordEl[i].value = passwords[i]
+        passwordEl[i].style.display = "block"
+
+    }
+}
+    */
